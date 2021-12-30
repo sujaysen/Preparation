e172c39e64f28f44b081472b2c3ea102e5e0ec5d
Sujay Sen <sujay.sen@justdial.com>
2021-12-30 11:44:16 +0530

    update

--- Backup/Notebook/read.py
+++ Backup/Notebook/read.py
@@ -1,8 +1,16 @@
 import json
-import json
 from datetime import datetime
 filename = datetime.today().strftime('%Y-%m')
 fp = open(filename,"r")
+total_expense = 0
+total_income = 0
 for r in fp.readlines():
-    print(json.loads(r))
+    data = json.loads(r)
+    if "expense" in data:
+      total_expense = total_expense + data["expense"]
+    if "income" in data:
+      total_income = total_income + float(data["income"])
+
+print("Total Expense = ",total_expense)
+print("Total Income = ",total_income)
 fp.close()
--- Backup/Notebook/write.py
+++ Backup/Notebook/write.py
@@ -1,12 +1,28 @@
 import json
 from datetime import datetime
 filename = datetime.today().strftime('%Y-%m')
+
 file_p = open(filename,"a+")
 data = {}
-data["date"] = datetime.today().strftime('%d')  
+
 while(int(input("Do you want to write? 1 for yes else 0 : "))):
   field = input("Enter the field name : ")
   value = input("Enter the value : ")
   data[field] = value
+
+expense = 0
+
+for res in data:
+  expense = expense + float(data[res])
+
+data["expense"] = expense
+data["income"] = float(input("Enter income : "))
+
+day = input("Enter date : ")
+if not day:
+  day = datetime.today().strftime('%d') 
+data["date"] = day
+
 file_p.writelines(json.dumps(data)+"\n")
+
 file_p.close()
