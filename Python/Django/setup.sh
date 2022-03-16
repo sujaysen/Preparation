98ea2bc6b53aaa6515830340fd03712c124654bb
sujay sen <sujay.alge@justdial.com>
2022-03-16 16:55:33 +0530

    add more files

---
+++ Python/Django/setup.sh
@@ -0,0 +1,14 @@
+# Step-1 : Install Python
+
+# Step-2 : Install Django
+sudo apt update
+sudo apt install python3-django
+django-admin --version
+
+# Step-3 : Install Database (MySql/MongoDb)
+
+# Step-4 : Create Project
+django-admin startproject myproject
+
+# Step-5 : Create Application
+python3 manage.py startapp myapp
---
+++ VIM/command.sh
@@ -0,0 +1,31 @@
+# Quit editor without saving changes
+:q!
+
+# Save changes and quit editor
+:wq
+
+# Insert after cursor
+i
+# Insert text at the beginning of line
+I
+
+# Append after cursor
+a
+
+# Append text at the end of line
+A
+
+# Open new line below cursor
+o
+
+# Open new line above cursor
+O
+
+# Delete char and insert 
+s
+
+
+
+
+
+
