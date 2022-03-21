f47c8986a97701dbf5827464fafbbdce1bafa645
sujay sen <sujay.alge@justdial.com>
2022-03-21 16:24:26 +0530

    update

---
+++ Linux/command.sh
@@ -0,0 +1,61 @@
+# Change directory
+cd path_directory
+# Nevigate to parent directory
+cd
+# Show files and folders
+ls
+ls -l
+ls -lrth
+# concate files
+cat file_name
+# create a new file without any content
+touch file_name
+# Create new directory
+mkdir directory_name
+# Get current working directory
+pwd
+# Remove file
+rm file_name
+# remove directory
+rm -r directory_name
+# download application/web pages directly from web
+wget url_name
+# Rename file/directory
+mv source_file dest_file
+mv -r source_file dest_file
+# Copy file/directory
+cp source_file dest_file
+cp -r source_file dest_file
+# view like tree
+tree
+tree -f # show in current direcory
+# head command
+head file_name
+# tail command
+tail file_name
+# wc Command
+wc filename
+# locate command
+locate file_name
+# copy from local to remote
+scp file.txt remote_username@ip:/remote_directory/newfilename.txt
+# Remote to local
+scp remote_username@10.10.0.2:/remote/file.txt /local/directory
+# remote to remote
+scp user1@host1.com:/files/file.txt user2@host2.com:/files	
+# show process
+ps -ef |grep "name of application"
+# kill process
+kill -9 process_id
+# check the connectivity between two nodes, that is whether the server is connected
+ping ip/domain
+# display the IP address for a given domain name
+host domain_name
+# to see date
+date
+# to see calender
+cal
+# check ip
+ifconfig
+
+
--- VIM/command.sh
+++ VIM/command.sh
@@ -1,31 +1,64 @@
 # Quit editor without saving changes
 :q!
-
 # Save changes and quit editor
 :wq
-
+ZZ
 # Insert after cursor
 i
 # Insert text at the beginning of line
 I
-
 # Append after cursor
 a
-
 # Append text at the end of line
 A
-
 # Open new line below cursor
 o
-
 # Open new line above cursor
 O
-
 # Delete char and insert 
 s
+# set indentation 
+open the file ~/.vimrc and the add following lines
+set tabstop=4
+syntax on " Syntax highlighting
+set showmatch " Shows matching brackets
+set ruler " Always shows location in file (line#)
+set smarttab " Autotabs for certain code
+set shiftwidth=4
+set autoindent
 
+# Set number into git add following line also
+set number
 
+# Vertically split
+:vsp file_name
 
+# Horizontally split
+:sp file_name
 
-
-
+# Cut the current line
+dd
+# Paste the selected text
+p
+# Copy the current line
+yy
+# Cut to EOL
+D
+# COPY TO EOL
+y$
+# Lowercase the entire line
+Vu
+# Upercase the entire line
+VU
+# Move the cursor to EOF
+G
+# Move the cursor to the beginning of the file
+gg
+# Saves lines 1 to 10 in myfile
+:1,10 w file_name
+# Appends lines 1 to 10 to myfile
+:1,10 w >> file_name
+# Inserts the content of myfile to current file
+:r file_name
+# Inserts the content of myfile under line 23
+:23r file_name
