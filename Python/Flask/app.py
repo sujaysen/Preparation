a2a6ac16c3a5f47010e47884407d6b4af6290820
sujay sen <sujay.alge@justdial.com>
2022-04-05 10:56:35 +0530

    update

--- Python/Flask/app.py
+++ Python/Flask/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
+from gevent.pywsgi import WSGIServer
 
 # creating the flask app
 app = Flask(__name__)
@@ -17,5 +18,6 @@ api.add_resource(Hello, '/api')
 
 # driver function
 if __name__ == '__main__':
-	app.run(debug = True,host='',port='5050')
+	http_server = WSGIServer(('127.0.0.1', 5050), app)
+	http_server.serve_forever()
 
