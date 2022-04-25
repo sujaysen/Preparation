73b56845557ed5da0840f92c38f6c92fa9167c16
sujay sen <sujay.alge@justdial.com>
2022-04-25 10:40:42 +0530

    update

--- Backup/ujjwal.py
+++ Backup/ujjwal.py
@@ -1,25 +1,30 @@
 from datetime import date
 import time
+
 Policy_holder = input("Enter the name of the policy holder : ") or "sujay"
-Agent_name = input("Enter the name of the agent : ") or "ujjwal"
+Agent_name = "ujjwal"
+
+def display_details(name):
+	if name == "sujay":
+		print("policy name = Jeevan Labh")
+		print("Policy_number = 402849906")
+		print("Date of Maturity = 30.05.2039")
+	elif name == "sulata":
+		print("policy name = New Endowment Plan-814")
+		print("Policy_number = 135905829")
+		print("Date of Maturity = 26.12.2031")
+	else:
+		pass
 
 if(Policy_holder == "sujay"):
-    if(Agent_name == "ujjwal"):
-        start_date = '29.08.2018'
-        end_date = '30.05.2033'
-    elif(Agent_name == "jagannath"):
-        start_date = '25.03.2019'
-        end_date = '26.12.2040'
-    else:
-        print("For sujay available agent name : ujjwal, bapi and jagannath")
-
+	start_date = '29.08.2018'
+	end_date = '30.05.2033'
 elif(Policy_holder == "sulata"):
-    start_date = ''
-    end_date = ''
+	start_date = '28.12.2016'
+	end_date = '26.12.2031'
 else:
-    pmode = int(input("if yearly press 1 or if half yearly press 2 or if quarterly press 4 : "))
-    start_date = input("Start Date(dd.mm.yyyy) : ")
-    end_date = input("End Date(dd.mm.yyyy) : ")
+	pass
+
 
 today = date.today()
 pattern = '%d.%m.%Y'
@@ -31,6 +36,7 @@ today_epoch = time.time()
 
 paid = 0
 Unpaid = 0
+display_details(Policy_holder)
 
 print("*************** LIST OF PAID INSTALLMENT ****************")
 while today_epoch>=start_date_epoch:
@@ -48,4 +54,3 @@ while end_date_epoch>=start_date_epoch:
 
 print("Total number of istallment done = ",paid)
 print("Total number of istallment due = ",Unpaid)
-print("Total amount paid = ",paid*30452)
